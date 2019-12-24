5604e252c32507ecd2f585b94ff91d31ee8bcb1f
Jor Sanders <jor.sanders@capgemini.com>
2019-12-24 10:40:22 +0100

    The Unit test are not broken at least

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, CounterComponent],
+      providers: [provideMockStore()]
     }).compileComponents();
   }));
 
@@ -24,12 +23,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ngrx-counter');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngrx-counter app is running!');
   });
 });
--- src/app/components/counter/counter.component.spec.ts
+++ src/app/components/counter/counter.component.spec.ts
@@ -1,23 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
 import { CounterComponent } from './counter.component';
+import { IAppState } from 'src/app/store/app-state.interface';
+import { Store } from '@ngrx/store';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
+  const initialState: IAppState = { count: { error: null, isActive: false, value: 0 } };
+  let store: Store<IAppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [CounterComponent],
+      providers: [provideMockStore({ initialState })]
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+
+    store = TestBed.get(Store);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
